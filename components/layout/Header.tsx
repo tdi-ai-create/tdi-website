074d24dc60d8a789dd009737489927e9131c392a
Hugh <raehughart@mac.lan>
2026-01-03 17:41:22 -0600

    Fix Header export

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
 
-export default function Header() {
+export function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
