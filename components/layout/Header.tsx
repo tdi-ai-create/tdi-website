6aadbfd2d0eb103b21bfd959faa316c87f8393f6
Hugh <raehughart@mac.lan>
2026-01-03 17:48:48 -0600

    Add Substack throughout site

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
 
-export function Header() {
+export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
