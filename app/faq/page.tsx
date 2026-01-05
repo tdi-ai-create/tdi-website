577bf189a4d7c202183540f58d3944e366bf1336
Hugh <raehughart@Raes-MacBook-Pro-2.local>
2026-01-04 20:44:07 -0600

    Move FAQ hero image up to top

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -91,7 +91,7 @@ export default function FAQPage() {
           className="absolute inset-0 bg-cover bg-fixed"
           style={{
             backgroundImage: 'url(/images/hero-faq.png)',
-            backgroundPosition: 'center 50%',
+            backgroundPosition: 'center 0%',
           }}
         />
         {/* Dark Overlay */}
