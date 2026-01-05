6e1c47c02f42c6e8b4f8a0f0531fd6072ef7d086
Hugh <raehughart@Raes-MacBook-Pro-2.local>
2026-01-04 21:10:22 -0600

    Move funding hero image to 100%

--- app/funding/page.tsx
+++ app/funding/page.tsx
@@ -15,7 +15,7 @@ export default function FundingPage() {
           className="absolute inset-0 bg-cover bg-fixed"
           style={{
             backgroundImage: 'url(/images/hero-funding.png)',
-            backgroundPosition: 'center 90%',
+            backgroundPosition: 'center 100%',
           }}
         />
         {/* Dark Overlay */}
